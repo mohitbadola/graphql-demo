df70d3e937a13be65cd30fc363faae6125089b6a
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-05-13 20:06:04 +0530

    Add GraphQL mutations for Player: create, update, delete

--- src/main/java/com/jason/graphql_demo/controller/PlayerController.java
+++ src/main/java/com/jason/graphql_demo/controller/PlayerController.java
@@ -1,8 +1,10 @@
 package com.jason.graphql_demo.controller;
 
 import com.jason.graphql_demo.model.Player;
+import com.jason.graphql_demo.model.Team;
 import com.jason.graphql_demo.service.PlayerService;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -22,10 +24,25 @@ public class PlayerController {
     private List<Player> findAll(){
         return playerService.findAll();
     }
-    
+
     @QueryMapping
     private Optional<Player> findOne(@Argument Integer id){
         return playerService.findOne(id);
+    }
+
+    @MutationMapping
+    private Player create(@Argument String name, @Argument Team team){
+        return playerService.create(name, team);
+    }
+
+    @MutationMapping
+    private Player update(@Argument Integer id, @Argument String name, @Argument Team team){
+        return playerService.update(id, name, team);
+    }
+
+    @MutationMapping
+    private Player delete(@Argument Integer id){
+        return playerService.delete(id);
     }
 
 }
