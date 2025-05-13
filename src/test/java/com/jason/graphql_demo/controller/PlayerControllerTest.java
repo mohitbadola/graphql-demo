3dc57fc433776b686dce051ee9c3d1b636d6e705
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-05-14 01:02:24 +0530

    Add Test findOne query with valid ID

--- src/test/java/com/jason/graphql_demo/controller/PlayerControllerTest.java
+++ src/test/java/com/jason/graphql_demo/controller/PlayerControllerTest.java
@@ -1,14 +1,15 @@
 package com.jason.graphql_demo.controller;
 
 import com.jason.graphql_demo.model.Player;
+import com.jason.graphql_demo.model.Team;
 import com.jason.graphql_demo.service.PlayerService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.graphql.test.tester.GraphQlTester;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @Import(PlayerService.class)
 @GraphQlTest(PlayerController.class)
@@ -35,4 +36,28 @@ class PlayerControllerTest {
                 .entityList(Player.class)
                 .hasSizeGreaterThan(9);
     }
+
+    @Test
+    void testValidIdShouldReturnPlayer(){
+        String document = """
+                query findOne($id: ID) {
+                  findOne(id: $id) {
+                    id
+                    name
+                    team
+                  }
+                }
+                """;
+
+        tester.document(document)
+                .variable("id", 1)
+                .execute()
+                .path("findOne")
+                .entity(Player.class)
+                .satisfies(player -> {
+                    Assertions.assertEquals("MS Dhoni", player.name());
+                    Assertions.assertEquals(Team.CSK, player.team());
+                });
+    }
+
 }
